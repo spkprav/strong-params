a86e7967e4635c88cc63c279505d9bc252315936
Kaarel Raspel <kaarel.raspel@gmail.com>
2016-08-17 11:39:48 +0300

    Reimplement strongify object as Parameters class
    Additionally:
      - package.json
        - bump version 0.7.0
        - add contributor
        - remove jasmine-node
        - add mocha, sinon & should
      - fix tests
      - replace jasmine with mocha

--- spec/middlewares/express.spec.js
+++ spec/middlewares/express.spec.js
@@ -1,95 +1,82 @@
-/* global describe, beforeEach, afterEach, expect, it */
+/* global describe, beforeEach, afterEach, it */
 var params = require('../..')
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
+var should = require('should')
 
 describe('expressMiddleware', function () {
+  var ctx
 
   beforeEach(function () {
-    this.port = 3001
-    this.url = 'http://localhost:' + this.port
-    this.app = express()
-    this.app.use(bodyParser.json())
-    this.app.use(bodyParser.urlencoded({ extended: true }))
-    this.app.use(params.expressMiddleware())
+    ctx = {}
+    ctx.port = 3001
+    ctx.url = 'http://localhost:' + ctx.port
+    ctx.app = express()
+    ctx.app.use(bodyParser.json())
+    ctx.app.use(bodyParser.urlencoded({ extended: true }))
+    ctx.app.use(params.expressMiddleware())
   })
 
   afterEach(function () {
-    if (this.server) {
-      this.server.close()
-    }
+    ctx.server.close()
   })
 
-  describe('this.params.all()', function () {
+  describe('req.parameters.all()', function () {
     it('should return `all` params', function (done) {
-      this.app.use(function (req, res, next) {
+      ctx.app.use(function (req, res, next) {
         res.json(req.parameters.all())
       })
-      this.server = this.app.listen(this.port)
+      ctx.server = ctx.app.listen(ctx.port)
 
-      request.post({ url: this.url + '/?p1=1&p2=2', form: { a1: 1, a2: 2 } }, function (err, res, body) {
-        expect(JSON.parse(body)).toEqual({ p1: '1', p2: '2', a1: '1', a2: '2' })
+      request.post({ url: ctx.url + '/?p1=1&p2=2', form: { a1: 1, a2: 2 } }, function (err, res, body) {
+        should(JSON.parse(body)).eql({ p1: '1', p2: '2', a1: '1', a2: '2' })
         done(err)
       })
     })
   })
 
-  describe('this.params.only()', function () {
-    it('should return `only` selected params', function (done) {
-      this.app.use(function (req, res, next) {
-        res.json(req.parameters.only('p1', ['a2']))
+  describe('req.parameters.permit()', function () {
+    it('should return `permit` selected params', function (done) {
+      ctx.app.use(function (req, res, next) {
+        res.json(req.parameters.permit('p1', 'a2').value())
       })
-      this.server = this.app.listen(this.port)
+      ctx.server = ctx.app.listen(ctx.port)
 
-      request.post({ url: this.url + '/?p1=1&p2=2', form: { a1: 1, a2: 2 } }, function (err, res, body) {
-        expect(JSON.parse(body)).toEqual({ p1: '1', a2: '2' })
+      request.post({ url: ctx.url + '/?p1=1&p2=2', form: { a1: 1, a2: 2 } }, function (err, res, body) {
+        should(JSON.parse(body)).eql({ p1: '1', a2: '2' })
         done(err)
       })
     })
 
   })
 
-  describe('this.params.except()', function () {
-
-    it('should return all params `except` those selected', function (done) {
-      this.app.use(function (req, res, next) {
-        res.json(req.parameters.except('p1', ['a2']))
-      })
-      this.server = this.app.listen(this.port)
-
-      request.post({ url: this.url + '/?p1=1&p2=2', form: { a1: 1, a2: 2 } }, function (err, res, body) {
-        expect(JSON.parse(body)).toEqual({ p2: '2', a1: '1' })
-        done(err)
-      })
-    })
-  })
-
-  describe('this.params.require()', function () {
+  describe('req.parameters.require()', function () {
 
     it('should return a `params` object of the required key', function (done) {
-      this.app.use(function (req, res, next) {
+      ctx.app.use(function (req, res, next) {
         res.json(req.parameters.require('p1').all())
       })
-      this.server = this.app.listen(this.port)
+      ctx.server = ctx.app.listen(ctx.port)
 
-      request.post({ url: this.url + '/?p1[s1]=1&p2=2', form: { p1: { s2: 2 }, a2: 2 } }, function (err, res, body) {
-        expect(JSON.parse(body)).toEqual({ s1: '1', s2: '2' })
+      request.post({ url: ctx.url + '/?p1[s1]=1&p2=2', form: { p1: { s2: 2 }, a2: 2 } }, function (err, res, body) {
+        should(JSON.parse(body)).eql({ s1: '1', s2: '2' })
         done(err)
       })
     })
 
     it('should throw an exception if the required key does not exist', function (done) {
-      this.app.use(function (req, res, next) {
+      ctx.app.use(function (req, res, next) {
         res.json(req.parameters.require('xx').all())
       })
-      this.app.use(function (req, res, next) {
+      ctx.app.use(function (err, req, res, next) {
+        should(err.message).equal('param `xx` required')
         res.status(500).send()
       })
-      this.server = this.app.listen(this.port)
+      ctx.server = ctx.app.listen(ctx.port)
 
-      request.post({ url: this.url + '/?p1=1', form: { a1: 1 } }, function (err, res, body) {
-        expect(res.statusCode).toEqual(500)
+      request.post({ url: ctx.url + '/?p1=1', form: { a1: 1 } }, function (err, res, body) {
+        should(res.statusCode).eql(500)
         done(err)
       })
     })
---
+++ spec/strongify.js
@@ -0,0 +1,297 @@
+/* global beforeEach, describe, it */
+var should = require('should')
+var sinon = require('sinon')
+
+var Parameters = require('..').strongify
+var PRIMITIVE_TYPES = [Boolean, Number, String, function Null () {
+  this.valueOf = function () { return null }
+}]
+
+describe('Parameters', function () {
+
+  describe('class methods', function () {
+
+    describe('_initValue', function () {
+      PRIMITIVE_TYPES.forEach(function (Primitive) {
+        it('should return primitive for ' + Primitive.name, function () {
+          // Prepare
+          var input = new Primitive().valueOf()
+          // Test
+          var result = Parameters._initValue(input)
+          // Verify
+          should(result).be.a[Primitive.name]()
+        })
+      })
+      it('should return instance of Parameters', function () {
+        // Prepare
+        var input = {}
+        // Test
+        var result = Parameters._initValue(input)
+        // Verify
+        result.should.be.a.instanceOf(Parameters)
+      })
+    })
+
+    describe('_isPrimitive', function () {
+      PRIMITIVE_TYPES.forEach(function (Primitive) {
+        it('should return true for primitive ' + Primitive.name + ' input', function () {
+          // Prepare
+          var input = new Primitive().valueOf()
+          // Test
+          var result = Parameters._isPrimitive(input)
+          // Verify
+          result.should.be.true()
+        })
+      })
+      it('should return false for not primitive Object input', function () {
+        // Prepare
+        var input = {}
+        // Test
+        var result = Parameters._isPrimitive(input)
+        // Verify
+        result.should.be.false()
+      })
+    })
+
+    describe('clone', function () {
+      it('should call instance `clone` for Parameters input', sinon.test(function () {
+        // Prepare
+        var params = new Parameters()
+        this.spy(params, 'clone')
+        // Test
+        var cloned = Parameters.clone(params)
+        // Verify
+        params.clone.calledOnce.should.be.true()
+        cloned.should.be.instanceOf(Parameters)
+        cloned.should.not.equal(params)
+      }))
+      it('should return input if it is not instance of Parameters', function () {
+        // Prepare
+        var primitive = Number()
+        // Test
+        var cloned = Parameters.clone(primitive)
+        // Verify
+        cloned.should.equal(primitive)
+      })
+    })
+
+    describe('_cloneArray', function () {
+      it('should return correctly', sinon.test(function () {
+        // Prepare
+        var cb = this.stub(Parameters, 'clone', function (input) { return input })
+        var input = [1, 3, 2]
+        // Test
+        var result = Parameters._cloneArray(input)
+        // Verify
+        result.should.eql(input)
+        cb.callCount.should.equal(3)
+      }))
+    })
+
+    describe('_cloneObject', function () {
+      it('should return correctly', sinon.test(function () {
+        // Prepare
+        var cb = this.stub(Parameters, 'clone', function (input) { return input })
+        var input = {
+          first: 1,
+          second: 3,
+          third: 2
+        }
+        // Test
+        var result = Parameters._cloneObject(input)
+        // Verify
+        result.should.eql(input)
+        cb.callCount.should.equal(3)
+      }))
+    })
+
+  })
+
+  describe('instance methods', function () {})
+
+  describe('operations', function () {
+
+    describe('constructing', function () {})
+    describe('cloning', function () {})
+    describe('whitelisting', function () {
+      var params
+
+      beforeEach(function () {
+        params = new Parameters({
+          primBoolean: true,
+          primNumber: 1,
+          primString: 'string',
+          array: [1, 3, 2],
+          objectArray: [
+            {
+              primBoolean: true,
+              primNumber: 1,
+              primString: 'string',
+              otherPrimary: 'other',
+              array: [1, 3, 2]
+            },
+            {
+              primBoolean: false,
+              primNumber: 2,
+              primString: '',
+              otherPrimary: 'other',
+              array: [4, 6, 5]
+            }
+          ],
+          objectNotatedArray: {
+            '0': {
+              primBoolean: true,
+              primNumber: 1,
+              primString: 'string',
+              otherPrimary: 'other'
+            },
+            '20': {
+              primBoolean: false,
+              primNumber: 2,
+              primString: '',
+              otherPrimary: 'other'
+            }
+          },
+          object: {
+            primBoolean: true,
+            primNumber: 1,
+            primString: 'string',
+            array: [1, 3, 2],
+            nestedObject: {
+              primBoolean: true,
+              primNumber: 1,
+              primString: 'string',
+              array: [1, 3, 2]
+            }
+          },
+          anotherObject: {
+            primBoolean: true,
+            primNumber: 1,
+            primString: 'string',
+            array: [1, 3, 2]
+          }
+        })
+      })
+
+      it('should whitelist primitives correctly', function () {
+        // Prepare
+        var filters = ['primBoolean', 'primNumber', 'primString']
+        // Test
+        var result = params.permit(filters).value()
+        // Verify
+        result.should.eql({
+          primBoolean: true,
+          primNumber: 1,
+          primString: 'string'
+        })
+      })
+
+      it('should whitelist primitives array correctly', function () {
+        // Prepare
+        var filters = [{array: []}]
+        // Test
+        var result = params.permit(filters).value()
+        // Verify
+        result.should.eql({
+          array: [1, 3, 2]
+        })
+      })
+
+      it('should whitelist nested object correctly', function () {
+        // Prepare
+        var filters = [{object: [{array: [], nestedObject: ['primBoolean', 'primNumber', 'primString']}]}]
+        // Test
+        var result = params.permit(filters).value()
+        // Verify
+        result.should.eql({
+          object: {
+            array: [1, 3, 2],
+            nestedObject: {
+              primBoolean: true,
+              primNumber: 1,
+              primString: 'string'
+            }
+          }
+        })
+      })
+
+      it('should whitelist object array correctly', function () {
+        // Prepare
+        var filters = [{objectArray: ['primBoolean', 'primNumber', 'primString', {array: []}]}]
+        // Test
+        var result = params.permit(filters).value()
+        // Verify
+        result.should.deepEqual({
+          objectArray: [
+            {
+              primBoolean: true,
+              primNumber: 1,
+              primString: 'string',
+              array: [1, 3, 2]
+            },
+            {
+              primBoolean: false,
+              primNumber: 2,
+              primString: '',
+              array: [4, 6, 5]
+            }
+          ]
+        })
+      })
+
+      it('should whitelist of same fingerprint object and array correctly', function () {
+        // Prepare
+        var filters = [{objectArray: ['primBoolean', 'primNumber', 'primString', {array: []}], anotherObject: ['primBoolean', 'primNumber', 'primString', {array: []}]}]
+        // Test
+        var result = params.permit(filters).value()
+        // Verify
+        result.should.deepEqual({
+          objectArray: [
+            {
+              primBoolean: true,
+              primNumber: 1,
+              primString: 'string',
+              array: [1, 3, 2]
+            },
+            {
+              primBoolean: false,
+              primNumber: 2,
+              primString: '',
+              array: [4, 6, 5]
+            }
+          ],
+          anotherObject: {
+            primBoolean: true,
+            primNumber: 1,
+            primString: 'string',
+            array: [1, 3, 2]
+          }
+        })
+      })
+
+      it('should whitelist object notated array correctly', function () {
+        // Prepare
+        var filters = [{objectNotatedArray: ['primBoolean', 'primNumber', 'primString']}]
+        // Test
+        var result = params.permit(filters).value()
+        // Verify
+        result.should.deepEqual({
+          objectNotatedArray: {
+            '0': {
+              primBoolean: true,
+              primNumber: 1,
+              primString: 'string'
+            },
+            '20': {
+              primBoolean: false,
+              primNumber: 2,
+              primString: ''
+            }
+          }
+        })
+      })
+    })
+
+  })
+
+})
